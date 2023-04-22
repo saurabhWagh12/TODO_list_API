d6e9cd1fc7788439a582ddbbe6d0868157bcc321
Saurabh Wagh <111182719+saurabhWagh12@users.noreply.github.com>
2023-04-22 17:43:07 +0530

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
-from app.models import *
 from todo.models import Task
-class CompanySerializers(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Company
-        fields = '__all__'
-
-class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Employee
-        fields = '__all__'        
-
+     
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
